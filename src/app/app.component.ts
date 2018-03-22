b90ca19844dfb368c0f1dc5e80ac287d524229d8
Vladimir Pavković <vladimirpavk@gmail.com>
2018-03-22 09:45:18 +0100

    ..to continue exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  public recipesVisible: boolean = true;
+  public shoppingListVisible: boolean = false;
+
+   public onRecipeItemClicked():void{
+    if(this.recipesVisible) return;
+    this.recipesVisible=false;
+    this.shoppingListVisible=true;
+  }
+
+  public onShopingListItemClicked():void{
+    if(this.shoppingListVisible) return;
+    this.shoppingListVisible=false;
+    this.recipesVisible=true;
+  }
+
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  public RecipeItemClicked:EventEmitter<void> = new EventEmitter<void>();
+  public ShopingListItemClicked:EventEmitter<void> = new EventEmitter<void>();
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  public onRecipeItemClicked():void{
+    this.RecipeItemClicked.emit();
+  }
+
+  public onShopingListItemClicked():void{
+    this.ShopingListItemClicked.emit();
+  }
 }

49406bf786dc611d2dbe70c2b333089b161d8a96
Vladimir Pavković <vladimirpavk@gmail.com>
2018-04-02 11:26:26 +0200

    +

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,20 @@
+import { Directive, ElementRef, Renderer2, OnInit, HostListener, HostBinding } from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective implements OnInit{
+  
+  @HostBinding('class.open') isDropDown: boolean = false;
+  
+  @HostListener('click') mouseClicked(value:Event){
+    this.isDropDown=!this.isDropDown;   
+  }
+
+  constructor() { }
+
+  ngOnInit(){
+    console.log("initialized");
+  }
+
+}

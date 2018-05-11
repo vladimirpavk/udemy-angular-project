ffac751bfc10c8476e735ac739498f2c72b3d13a
Vladimir Pavković <vladimirpavk@gmail.com>
2018-05-11 10:50:25 +0200

    +

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { HomeComponent } from './core/home/home.component';
 
 const appRoutes:Route[] = [
     { path: 'home', component: HomeComponent },
-    //{ path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule'},
+    { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule'},
     { path: 'shoppinglist', component: ShoppingListComponent },
     { path: '', redirectTo: 'home', pathMatch: "full"}   
 ];

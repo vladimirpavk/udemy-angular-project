f3a54854d7c283148279c95d918e1b6de0b1b4c5
Vladimir Pavković <vladimirpavk@gmail.com>
2018-07-19 22:54:37 +0200

    +

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -4,6 +4,10 @@ import { ShoppingListService } from '../../shopping-list/services/shopping-list.
 import { Ingridient } from '../../shared/ingridient.model';
 import { ActivatedRoute, Data, Params, Router } from '@angular/router';
 import { RecipeService } from '../services/recipe.service';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../store/app.reducers';
+import { Observable } from 'rxjs/Observable';
+import { take } from 'rxjs/operators'
 
 @Component({
   selector: 'app-recipe-detail',
@@ -13,29 +17,40 @@ import { RecipeService } from '../services/recipe.service';
 export class RecipeDetailComponent implements OnInit {
 
   public recipe: Recipe;
+  public recipeState:Observable<AppState["recipeState"]>;
+
   public recipeId:number;
 
   constructor(private shoppingListService: ShoppingListService, 
     private route: ActivatedRoute, 
     private recipeService: RecipeService,
-    private router:Router) { }
+    private router:Router,
+    private store:Store<AppState>
+  ) { }
 
   ngOnInit() {   
     this.route.params.subscribe((params:Params)=>{  
       this.recipeId=+params['id'];     
-      this.recipe=this.recipeService.getRecipeById(this.recipeId);      
+      //when using services uncomment
+      //this.recipe=this.recipeService.getRecipeById(this.recipeId);
+      this.recipeState = this.store.select('recipeState');
+      this.recipeState.take(1).subscribe(
+        (state:AppState["recipeState"])=>{
+          this.recipe=state.recipes[+params['id']];
+        }
+      );
     });
   }
 
   deleteRecipe(){
-    this.recipeService.deleteRecipe(this.recipe);
-    this.router.navigate(['../']);
+    /*this.recipeService.deleteRecipe(this.recipe);
+    this.router.navigate(['../']);*/
   }
 
   private toShoppingListClicked():void{
-    for(let Aing of this.recipe.ingridients){      
+    /*for(let Aing of this.recipe.ingridients){      
       this.shoppingListService.addIngridient(Aing);
-    }
+    }*/
   }
 
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -3,20 +3,22 @@ import { Recipe } from '../recipe.model';
 import { RecipeService } from '../services/recipe.service';
 import { Subscription } from 'rxjs/Subscription';
 import { Store } from '@ngrx/store';
-import * as RecipeState from '../store/recipes.reducer';
+import { AppState } from '../../store/app.reducers';
+import { Observable } from 'rxjs/Observable'
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit, OnDestroy {
-
-  public recipes: Recipe[];
+export class RecipeListComponent implements OnInit{
+  //when using services
+  //public recipes: Recipe[];
+  public recipes:Observable<AppState["recipeState"]>;
 
   private subs:Subscription;
 
-  constructor(private recipeService:RecipeService, private store: Store<RecipeState.RecipesState>){    
+  constructor(private recipeService:RecipeService, private store: Store<AppState>){    
   }
 
   ngOnInit() {
@@ -29,10 +31,6 @@ export class RecipeListComponent implements OnInit, OnDestroy {
       }
     );
     this.recipes=this.recipeService.getRecipes();*/
+    this.recipes = this.store.select("recipeState");
   }
-
-  ngOnDestroy(){
-    this.subs.unsubscribe();
-  }
-
 }

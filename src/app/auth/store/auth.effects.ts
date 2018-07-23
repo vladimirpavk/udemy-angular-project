9daec8a4cab8e3dccd4d718432876e42056527bf
Vladimir Pavković <vladimirpavk@gmail.com>
2018-07-23 22:53:04 +0200

    +

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -45,89 +45,17 @@ export class AuthEffects{
             return (<AuthActions.TrySignInUser>action).payload;
         }),
         switchMap((payload: {username:string, password:string})=>{
-            return fromPromise(firebase.auth().signInWithEmailAndPassword(payload.username, payload.password));
-        }),
-        switchMap((user:any)=>{
-            return [ 
-                {
-                    "type": AuthActions.FIRST_TIME_SIGN_IN_USER
-                },
-                {
-                    "type": AuthActions.SIGN_IN_USER
-                },
-                {
-                    "type": AuthActions.SET_TOKEN,
-                    "payload": user.G
-                }
-            ]
-        }),
-        catchError((err:any)=>{
-            return of(new AuthActions.FirstTimeSignInUser());
-        })
-
-   /* @Effect() public authSignin =  this.actions.ofType(AuthActions.TRY_SIGN_IN_USER)
-        .map((action: AuthActions.TrySignInUser)=>{
-            console.log("First step");
-            return (<AuthActions.TrySignInUser>action).payload
-        })
-        .mergeMap((payload: {username:string, password:string})=>{
-            return firebase.auth().signInWithEmailAndPassword(payload.username, payload.password)
-               .then((user:any)=>{
-                return Observable.from([ 
-                    {
-                        "type": AuthActions.FIRST_TIME_SIGN_IN_USER
-                    },
-                    {
-                        "type": AuthActions.SIGN_IN_USER
-                    },
-                    {
-                        "type": AuthActions.SET_TOKEN,
-                        "payload": user.G
-                    }
-                ])
-                /*return{
-                    type:AuthActions.SET_TOKEN,
-                    payload: user.G
-                }          
-               })
-               .catch((err)=>{
-                   return{
-                       type:AuthActions.FIRST_TIME_SIGN_IN_USER
-                   }
-               })
-        });*/
-        
-        
-        
-        /*.switchMap(
-            (payload:{username:string, password:string})=>{
-                console.log("Trying to signin");
-               // return fromPromise(firebase.auth().signInWithEmailAndPassword(payload.username, payload.password));
-               firebase.auth().signInWithEmailAndPassword(payload.username, payload.password)
-               .then(()=>{
-
-               })
-               .catch((err)=>{
-                   console.log(err);
-               })
-            }
-        )    
-        .switchMap(()=>{
-            console.log("Switch map");
-            return fromPromise(firebase.auth().currentUser.getIdToken());
-        })
-        .mergeMap((token:string)=>{      
-            console.log("Where the token is: " + token);
-            return [
-                {
-                    type:AuthActions.SIGN_IN_USER
-                },
-                {
-                    type:AuthActions.SET_TOKEN,
-                    payload: token
-                }
-            ]
-        });*/
-
-    
+            return fromPromise(firebase.auth().signInWithEmailAndPassword(payload.username, payload.password)).pipe(
+                switchMap((user:any)=>{
+                    return [ 
+                        new AuthActions.FirstTimeSignInUser(),
+                        new AuthActions.SignInUser(payload.username, payload.password),
+                        new AuthActions.SetToken(user.G)
+                    ]
+                }),
+                catchError((err:any)=>{
+                    return of(new AuthActions.FirstTimeSignInUser());
+                })
+            )
+        }));
 }
